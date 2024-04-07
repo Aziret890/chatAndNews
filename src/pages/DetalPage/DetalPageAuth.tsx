2261b37e962adbe6583900a0ab9451bb8abfaaf4
aziret <kubaaziret68@gmail.com>
2024-04-07 13:55:22 +0600

    started project

---
+++ src/pages/DetalPage/DetalPageAuth.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DetalPageAuth() {
+  return (
+    <div>DetalPageAuth</div>
+  )
+}
+
+export default DetalPageAuth
---
+++ src/pages/NotFount/NotFount.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NotFount() {
+  return (
+    <div>NotFount</div>
+  )
+}
+
+export default NotFount
