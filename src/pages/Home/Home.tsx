9e693cd7a6a8a7215e8b23da274470d32396558a
mairambek <mairambekjoldoshbekov30@gmail.com>
2024-04-08 19:33:30 +0600

    finished profile

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,9 @@
 import Header from "../../components/header/Header";
-import Header2 from "../../components/header2/header2";
 
 function Home() {
   return (
     <>
       <Header />
-      <br />
-      <br />
-      <Header2 />
     </>
   );
 }
