e272d7d1be1c2e687d41c00c2a799b60345787ed
Your Name <your_email@example.com>
2024-04-09 13:36:28 +0600

    finished news

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+// import { Img } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <>
-    <h1>vnweopv </h1>
-  </>;
+  return
 }
 
 export default App;
